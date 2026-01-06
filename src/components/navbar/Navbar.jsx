1d5a03f4245753fc9e53db7f5a6c6e4fa20d36ce
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-06 11:16:21 +0545

    navbar sticky

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../assets/logo.png'
 import "./navbar.css"
 const Navbar = () => {
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 500 ? setSticky(true) : setSticky(false);
+        })
+    }, [])
     return (
-        <nav className='container'>
+        <nav className={` container ${sticky ? "dark-nav" : " "}`} >
             <img src={logo} alt="" className='logo' />
             <ul >
                 <li>Home</li>
@@ -13,7 +20,7 @@ const Navbar = () => {
                 <li>Testimonials</li>
                 <li><button className='btn'>Contact us</button></li>
             </ul>
-        </nav>
+        </nav >
     )
 }
 
