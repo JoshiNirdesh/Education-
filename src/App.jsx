38e70c83ab986434cd6d45a46cb6fb35e4f98ddd
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-06 09:30:38 +0545

    program

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
 import Hero from './components/hero/Hero'
+import Program from './components/program/Program'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className='container'>
+        <Program />
+      </div>
     </div>
   )
 }
